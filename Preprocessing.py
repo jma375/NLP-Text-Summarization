d1e348b71d80e358fdfbd8092881c7e280405b83
jma375 <jma375@miami.edu>
2022-12-04 16:05:07 -0500

    refactoring functions

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,4 +1,5 @@
 #James Alfano
+import re
 import pandas as pd
 import numpy as np
 import sys
@@ -20,41 +21,60 @@ from datasets import Dataset
 #Each article begins with a location and source
 #e.g. "MINNEAPOLIS, Minnesota (CNN) " or "WASHINGTON (CNN)"
 #This function seeks to remove the intro
-def rvm_article_intro(df):
-    for i in range(len(df)):
-        df.iloc[i,0] = df.iloc[i,0][df.iloc[i,0].find('(cnn)')+5:] #Looks to remove "(CNN)" and everything before
-
+#def rvm_article_intro(df):
+    #for i in range(len(df)):
+        #df.iloc[i,0] = df.iloc[i,0][df.iloc[i,0].find('(cnn)')+5:] #Looks to remove "(CNN)" and everything before
+    #for i in range(len(dataset["article"])):
+    #    dataset["article"][i] = dataset["article"][i][dataset["article"][i].find('(cnn)')+5:]
+def rvm_article_intro(dataset):
+    return {"article": dataset["article"][dataset["article"].find('(cnn)')+5:]}
 
 #Expand contractions 
 #e.g. they're -> they are
-def expand_contractions(df):
-    for i in range(len(df)):
-        df.iloc[i,0] = contractions.fix(df.iloc[i,0])
+#def expand_contractions(dataset):
+    #for i in range(len(dataset["article"])):
+        #df.iloc[i,0] = contractions.fix(df.iloc[i,0])
+        #dataset["article"][i] = contractions.fix(dataset["article"][i])
+def expand_contractions(dataset):
+    return {"article": contractions.fix(dataset["article"])}
 
 
 #Lower Case Function
-def lower_case_text(df):
-    df["text"] = df["text"].str.lower()
+def lower_case_text(dataset):
+    #df["text"] = df["text"].str.lower()
+    #df["train"]["article"] = df["train"]["article"].str.lower()
+    #dataset["article"] = dataset["article"].str.lower()
+    return {"article": dataset["article"].lower()}
 
 
 #Function to tokenize df 
-def token(df):
-    df['text'] = df['text'].apply(nltk.word_tokenize)
-    df['y'] = df['y'].apply(nltk.word_tokenize)
+#def token(df):
+    #df['text'] = df['text'].apply(nltk.word_tokenize)
+    #df['y'] = df['y'].apply(nltk.word_tokenize)
+    #df["text"] = df["text"].str.findall(r"[\w']+|[.,!?;]")
+    #df["y"] = df["y"].str.findall(r"[\w']+|[.,!?;]")
+    #dataset["article"] = dataset["article"].str.findall(r"[\w']+|[.,!?;]")
+    #dataset["highlights"] = dataset["article"].str.findall(r"[\w']+|[.,!?;]")
+def split_str(dataset):
+    return {"article": re.findall(r"[\w']+|[.,!?;]",dataset["article"])}
+    
 
 
 #Removing special characters
 #e.g. "'","``","`","-", and "--"
-def rmv_special_chars(df):
-    char_rmv = ["'","``","`","-", "--","''",":","'s","$","(",")","?",".",",","’","‘"]
-    df['text'] = df['text'].apply(lambda x: [item for item in x if item not in char_rmv])
-
+#def rmv_special_chars(df):
+    #char_rmv = ["'","``","`","-", "--","''",":","'s","$","(",")","?",".",",","’","‘"]
+    #df['text'] = df['text'].apply(lambda x: [item for item in x if item not in char_rmv])
+    #dataset['article'] = dataset['article'].apply(lambda x: [item for item in x if item not in char_rmv])
+def rmv_special_chars(dataset):
+    return {"article": re.sub('[^A-Za-z0-9 ]+', '',dataset["article"])}
 
 #Function to lemmatize 
 #i.e. transform the word to its root, or lemma 
-def lemmatize_wrds(df):
-    wnl = WordNetLemmatizer()
-    df['text'] = df['text'].apply(lambda lst:[wnl.lemmatize(word) for word in lst])
+#def lemmatize_wrds(df):
+#    wnl = WordNetLemmatizer()
+#    #df['text'] = df['text'].apply(lambda lst:[wnl.lemmatize(word) for word in lst])
+#    dataset['article'] = dataset['article'].apply(lambda lst:[wnl.lemmatize(word) for word in lst])
 
 
 #Function to get number average number of words in article/summary
@@ -67,7 +87,14 @@ def avg_num_words(df):
 
 
 #Function to plot Average Number of words in Summaries and Articles 
-def plot_counts(df):
+def plot_counts(dataset,split):
+    
+    #Convert dataset to df
+    splt_data = dataset.map(split_str)
+    lst_dics = [dic for dic in splt_data[split]]
+    df = pd.DataFrame(lst_dics).rename(columns={"article":"text", 
+      "highlights":"y","id":"id"})[["text","y","id"]]
+
     #get average and list of word counts  
     avg_smry, smry_counts = avg_num_words(df["y"])
     avg_text, text_counts = avg_num_words(df["text"])
@@ -93,10 +120,17 @@ def plot_counts(df):
     plt.axvline(avg_text, color='k', linestyle='dashed', linewidth=1, label = "Average Count = {0}".format(avg_text)) #Add Average to hist
     plt.legend()
     plt.show
+    return avg_smry, smry_counts, avg_text, text_counts
 
 
 #Function to plot word frequencies 
-def wrd_freq_plot(df,title):
+def wrd_freq_plot(dataset,split,title):
+    #Convert dataset to df
+    splt_data = dataset.map(split_str)
+    lst_dics = [dic for dic in splt_data[split]]
+    df = pd.DataFrame(lst_dics).rename(columns={"article":"text", 
+      "highlights":"y","id":"id"})[["text","y","id"]]
+    
     #Set size
     fig = plt.figure(figsize=(8, 6))
     temp = df.text.explode().to_frame().reset_index(drop=True)
@@ -113,18 +147,19 @@ def wrd_freq_plot(df,title):
 
 
 #Function to aggregate all preprocessing functions into one
-def preprocess(df, type):
-    expand_contractions(df)
-    lower_case_text(df)
-    rvm_article_intro(df)
-    token(df)
-    rmv_special_chars(df)
-    wrd_freq_plot(df,type)
+def preprocess(dataset):
+    dataset = dataset.map(expand_contractions)#expand_contractions(df)
+    dataset = dataset.map(lower_case_text)#lower_case_text(df)
+    dataset = dataset.map(rvm_article_intro) #rvm_article_intro(df)
+    dataset = dataset.map(rmv_special_chars) #rmv_special_chars(df)
+    return dataset
+    #temp = dataset.map(split_str) #token(df)
+    #wrd_freq_plot(df,type)
     #lemmatize_wrds(df)
     
 
 
-#Recombine data to be in correct format
+""" #Recombine data to be in correct format
 def combine_data(subset_train,subset_test,subset_valid):
     #Here we rejoin the texts    
     subset_valid["text"] = subset_valid["text"].apply(lambda x: ' '.join(wrd for wrd in x))
@@ -144,3 +179,4 @@ def combine_data(subset_train,subset_test,subset_valid):
 
     dataset = DatasetDict(d)
     return dataset
+ """
